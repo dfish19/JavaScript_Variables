9b6601d456c0a76e283450e88e90f3242747f6f9
dfish19 <dfish19@yahoo.com>
2022-09-14 14:16:28 -0500

    initial

--- app.js
+++ app.js
@@ -4,19 +4,23 @@ console.log(
 );
 
 // Exercise 1
-const firstName = Donte;
+const firstName = "Donte";
 let lastName = "Fisher";
 var age = 39;
+console.log(`${firstName} ${lastName} is ${age}.`);
 
 // Exercise 2
-firstName = "Donte";
+firstName1 = "Donte";
 lastName = "Fisher";
 age = 39;
+console.log(`${firstName} and ${lastName} is my surname!`);
+
 
 // Exercise 3
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+console.log(`${language} has been around since ${createdYear}!`);
 
 // Exercise 4
 let price = 19.99
@@ -25,6 +29,7 @@ let salesPercentage = 15;
 let stock = 0;
 let instock = false;
 let selectedSize = "M";
+console.log("The cost of the groceries was about average.")
 
 //Exercise 5
 let title = "Name of the Wind";
@@ -32,3 +37,4 @@ let author = "Patrick Rothfuss";
 let pageCount = 722;
 let bookmark = 456;
 let hasRead = true;
+console.log("I do not like to read");
