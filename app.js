94df48ad7efb86f7a9e451a3a7148cc0f52dc9d3
dfish19 <dfish19@yahoo.com>
2022-09-14 12:24:28 -0500

    initial

--- app.js
+++ app.js
@@ -4,7 +4,31 @@ console.log(
 );
 
 // Exercise 1
+const firstName = Donte;
+let lastName = "Fisher";
+var age = 39;
 
 // Exercise 2
+firstName = "Donte";
+lastName = "Fisher";
+age = 39;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99
+let onsale = false;
+let salesPercentage = 15;
+let stock = 0;
+let instock = false;
+let selectedSize = "M";
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
